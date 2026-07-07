c4465a40f4fe0acd238d5d3678e3cb52a681334f
bjarneksr <bjarnekaiser2201@gmail.com>
2026-07-07 14:15:36 +0200

    Preparation for initial deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,16 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import { clerkMiddleware } from '@clerk/express'
+import fs from 'fs'
+import path from 'path'
 
 import { connectDB } from './lib/db.js';
 
 const app = express();
 const PORT = process.env.PORT
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), 'public');
 
 app.use(express.json());
 app.use(cors({origin:FRONTEND_URL, credentials:true}));
@@ -17,6 +21,12 @@ app.get('/test', (req, res) => {
     res.status(200).json({message: 'running'});
 })
 
+if (fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+    app.get('/{*any}', (req, res, next) => {
+        res.sendFile(path.join(publicDir, 'index.html'), (error) => next(error));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}!`)
