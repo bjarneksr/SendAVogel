ce30b70e240240bee6dcdf18cb4ca5ad6603cdab
bjarneksr <bjarnekaiser2201@gmail.com>
2026-07-08 18:15:17 +0200

    Added clerk webhook to process user events and update database

---
+++ backend/src/webhooks/clerk.js
@@ -0,0 +1,53 @@
+import express from 'express'
+import User from '../models/User.js'
+import { veryfyWebhook } from '@clerk/backend/webhooks'
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    try {
+        
+        const signingSecret = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
+        if (!signingSecret) {
+            res.status(503).json({message: 'Webhook secret is not provided'});
+        }
+
+        const payload = Buffer.isBuffer(req.body) ? req.body.toString('utf8') : String(req.body);
+        const request = new Request('http://internal/webhooks/clerk', {
+            method: 'POST',
+            headers: new Headers(req.header),
+            body: payload,
+        });
+        
+        const evt = await veryfyWebhook(request, { signingSecret });
+        
+        if (evt.type === 'user.created' || evt.type === 'user.updated') {
+            const u = evt.data;
+            
+            const email =
+            u.email_addresses?.find((e) => e.id === u.primary_email_address_id)?.email_address ??
+            u.email_addresses?.[0]?.email_address;
+            
+            const userName =
+            [u.first_name, u.last_name].filter(Boolean).join(" ") || u.username || email?.split("@")[0];
+            
+            await User.findOneAndUpdate(
+                { clerkId: u.id },
+                { clerkId: u.id, email, userName, profilePic: u.image_url },
+                { new: true, upsert: true, setDefaultsOnInsert: true },
+            );
+        }
+        
+        if (evt.type === "user.deleted") {
+            if (evt.data.id) await User.findOneAndDelete({ clerkId: evt.data.id });
+        }
+
+        res.status(200).json({ received: true });
+    } catch (error) {
+        res.status(400).json({ message: 'Webhook verification failed'});
+    }
+    
+    
+})
+
+export default router;
